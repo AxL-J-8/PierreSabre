7a472532be563e26007c01f861d0266d2ec5b111
joseph remy <jsr3999a@ens.fsi>
2022-10-27 14:18:30 +0200

    creation de classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,61 @@
+package personnages;
+
+public class Humain {
+  private String boisson;
+  private String nom;
+  private int argent;
+  
+public Humain(String nom,String boisson,int argent) {
+	this.argent=argent;
+	this.boisson=boisson;
+	this.nom=nom;
+}
+
+
+public String getNom() {
+	return nom;
+}
+
+
+public int getArgent() {
+	return argent;
+}
+
+
+public void direBonjour() {
+	parler("Bonjour ! Je m’appelle " +nom +" et j’aime boire du "+ boisson);
+}
+
+
+public void boir() {
+	parler("Mmmm, un bon verre de "+boisson+" ! GLOUPS !");
+}
+
+
+public void acheter(String bien, int prix) {
+	String bienEtPrix= ("m'offrir un "+bien+" à "+prix+ " sous");
+    String argentEnPoche= ( argent+" sous en poche. ");
+    
+	if (prix <=argent) {
+		parler("J'ai "+argentEnPoche+"Je vais pouvoir "+bienEtPrix);
+		perdreArgent(prix);
+	}
+	else {
+		parler("Je n'ai plus que "+argentEnPoche+"Je ne peux meme pas "+bienEtPrix);
+	}
+}
+
+public void gagnerArgent(int gain) {
+	argent+=gain;
+}
+
+
+public void perdreArgent(int perte) {
+	argent-=perte;
+}
+
+private void parler(String texte) {
+	System.out.println("("+nom+")"+"-"+texte);
+}
+
+}
